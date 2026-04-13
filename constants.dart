08120c6c6ee71c6a8b1ccd95ff9efc96c85a3b98
merajtahmed <merajtahmed2022@gmail.com>
2026-04-14 00:25:43 +0600

    Add files via upload

---
+++ constants.dart
@@ -0,0 +1,7 @@
+class AppConstants {
+  static const String openAiUrl = 'https://openrouter.ai/api/v1/chat/completions';
+  static const String openAiModel = 'openai/gpt-oss-20b:free';
+  static const String freeAiModel = 'google/gemma-7b-it:free';
+  static const String lottieUrl =
+      'https://assets10.lottiefiles.com/packages/lf20_jcikwtux.json';
+}
